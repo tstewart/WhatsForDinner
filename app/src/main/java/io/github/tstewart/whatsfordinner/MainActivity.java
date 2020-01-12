745bdd5f9e3817703b38593e3905c63bee459e32
T. Stewart <tomstewart20@gmail.com>
2020-01-12 20:22:33 +0000

    Implemented activities closes #2

--- app/src/main/java/io/github/tstewart/whatsfordinner/MainActivity.java
+++ app/src/main/java/io/github/tstewart/whatsfordinner/MainActivity.java
@@ -2,6 +2,8 @@ package io.github.tstewart.whatsfordinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topbar_with_settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
